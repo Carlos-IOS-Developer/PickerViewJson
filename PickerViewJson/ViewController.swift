a8a07d883c52f9132c716b37559ab6288b643fb1
carlos-ios-developer <dr.carlos.iosdeveloper@gmail.com>
2018-01-21 10:58:33 +0200

    upload to git

--- PickerViewJson/ViewController.swift
+++ PickerViewJson/ViewController.swift
@@ -8,16 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Country: Decodable {
+    let name: String
+}
+
+class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource  {
+    @IBOutlet weak var countrylbl: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var countries = [Country]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://restcountries.eu/rest/v2/all")
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error == nil {
+                do{
+                    self.countries = try JSONDecoder().decode([Country].self, from: data!)
+                }catch {
+                    print("Parse Error")
+                }
+                
+                DispatchQueue.main.async {
+                    self.pickerView.reloadComponent(0)
+                }
+                
+            }
+            }.resume()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   // PickerView methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row].name
+    }
+    
+    // delegate method
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCountry = countries[row].name
+        countrylbl.text = selectedCountry
     }
 
 
